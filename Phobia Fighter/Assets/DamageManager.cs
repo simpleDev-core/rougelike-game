bc9081f1f6781986116a7909c39260b490c9862a
Isa <isaiah.j.murray@gmail.com>
2022-05-19 09:02:18 -0400

    Damage Manager implemented

---
+++ Phobia Fighter/Assets/DamageManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageManager : MonoBehaviour
+{
+    public float damage = 1;
+    public bool continuous = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<healthManager>() != null)
+        {
+            collision.gameObject.GetComponent<healthManager>().health -= damage;
+        }
+    }
+}
