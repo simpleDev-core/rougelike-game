33ecfcd3eb23a2753f91d2524f1a4e1cbaac92b2
Isa <isaiah.j.murray@gmail.com>
2022-05-19 23:42:07 -0400

    stuff added

--- Phobia Fighter/Assets/SwordManager.cs
+++ Phobia Fighter/Assets/SwordManager.cs
@@ -9,6 +9,11 @@ public class SwordManager : MonoBehaviour
     public swordType sword;
     public Vector2 hitboxDimensions;
     public Animator animator;
+    Vector3 mouse_pos;
+    public Transform target; //Assign to the object you want to rotate
+    Vector3 object_pos;
+    float angle;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +23,14 @@ public class SwordManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        mouse_pos = Input.mousePosition;
+        mouse_pos.z = 5.23f; //The distance between the camera and object
+        object_pos = Camera.main.WorldToScreenPoint(target.position);
+        mouse_pos.x = mouse_pos.x - object_pos.x;
+        mouse_pos.y = mouse_pos.y - object_pos.y;
+        angle = Mathf.Atan2(mouse_pos.y, mouse_pos.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+
         if (Input.GetMouseButtonDown(0))
         {
             animator.Play("Slash");
