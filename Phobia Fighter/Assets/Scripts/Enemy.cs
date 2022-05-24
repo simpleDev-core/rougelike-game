454256c73d7be5ee2fca64a3c98ef232fb6b47f6
Isa <isaiah.j.murray@gmail.com>
2022-05-24 09:17:50 -0400

    invientorein

--- Phobia Fighter/Assets/Scripts/Enemy.cs
+++ Phobia Fighter/Assets/Scripts/Enemy.cs
@@ -11,10 +11,17 @@ public class Enemy : MonoBehaviour
     Vector2 CurrentSpeed;
     Vector2 OldSpeed;
     Vector2 target;
+    public enum attackType {Lunge, Projectile, Contact, SpinLaser}
+    public attackType Attack;
+    public float moveRange = 0.1f;
     bool walk = true;
+    
     float dragFactor = 0.75f;
     public float VisionRange = 10;
     public int trackDestination = -1;
+    public float attackCooldown = 1;
+    public float attackWindup = 0.5f;
+    bool cooldownAttack;
     List<Vector2> visibleTracks = new List<Vector2>();
     bool locked;
     bool tracking;
@@ -24,13 +31,19 @@ public class Enemy : MonoBehaviour
     public bool debug;
     List<Vector2> TrackedPositions = new List<Vector2>();
     public LayerMask IgnoreMe;
+    LineRenderer laserLine;
     // Start is called before the first frame update
     void Start()
     {
+        if(Attack == attackType.SpinLaser)
+        {
+            laserLine = GetComponent<LineRenderer>();
+        }
         target = gameObject.transform.position;
         rb = GetComponent<Rigidbody2D>();
         player = GameObject.FindGameObjectWithTag("Player");
         gameObject.transform.parent = null;
+        gameObject.transform.rotation = Quaternion.FromToRotation(transform.up, Vector2.up);
         
     }
 
@@ -55,12 +68,77 @@ public class Enemy : MonoBehaviour
         yield break;
     }
 
+    IEnumerator Lunge(Vector2 lunge, float windup)
+    {
+        StartCoroutine(stun(windup+1));
+        yield return new WaitForSeconds(windup);
+        rb.velocity += lunge;
+        yield break;
 
+    }
+
+    IEnumerator AttackCooldown(float time)
+    {
+        cooldownAttack = true;
+        yield return new WaitForSeconds(time);
+        cooldownAttack = false;
+    }
     // Update is called once per frame
     void Update()
     {
+
+
+
+
+
+        if (Attack == attackType.SpinLaser && Vector2.Distance(player.transform.position, transform.position) <= 16)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(gameObject.transform.position, new Vector2(Mathf.Cos(Time.time), Mathf.Sin(Time.time)),Mathf.Infinity, layerMask:~IgnoreMe);
+            if(hit.collider != null)
+            {
+                Debug.DrawLine(transform.position, hit.point);
+                laserLine.SetPosition(0, transform.position);
+                laserLine.SetPosition(1, hit.point);
+                if(hit.collider.gameObject.GetComponent<healthManager>() != null)
+                {
+                    hit.collider.gameObject.GetComponent<healthManager>().Damage(10*Time.deltaTime);
+                }
+            }
+            else
+            {
+                Debug.DrawLine(transform.position, new Vector2(Mathf.Cos(Time.time), Mathf.Sin(Time.time))*100);
+                laserLine.SetPosition(0, transform.position);
+                laserLine.SetPosition(1, new Vector3(Mathf.Cos(Time.time), Mathf.Sin(Time.time),0)*100+gameObject.transform.position);
+            }
+        }
         if (walk)
         {
+            
+            if (Vector2.Distance(player.transform.position, gameObject.transform.position) <= moveRange && !cooldownAttack)
+            {
+                StartCoroutine(AttackCooldown(attackCooldown));
+                if (Attack == attackType.Lunge)
+                {
+                    StartCoroutine(Lunge((player.transform.position-transform.position)*10,attackCooldown));
+                }
+                if (Attack == attackType.Projectile)
+                {
+                    StartCoroutine(stun(1));
+                }
+                
+            }
+
+
+
+
+
+
+
+
+
+
+
+
             rb.velocity *= dragFactor;
             RaycastHit2D hit= Physics2D.Raycast(gameObject.transform.position, (player.transform.position-gameObject.transform.position), Mathf.Infinity, ~IgnoreMe);
             if (hit.collider.gameObject.tag == "Player" && Vector2.Distance(transform.position,player.transform.position)<= VisionRange)
@@ -110,7 +188,7 @@ public class Enemy : MonoBehaviour
                     }
                     else
                     {
-                        if (Vector2.Distance(transform.position, target) <= 1)
+                        if (Vector2.Distance(transform.position, target) <= moveRange)
                         {
                             //print("nextDestination");
                             if (trackDestination < TrackedPositions.Count)
@@ -196,14 +274,14 @@ public class Enemy : MonoBehaviour
 
     public void Stun()
     {
-        StartCoroutine(stun());
+        StartCoroutine(stun(0.75f));
     }
 
-    IEnumerator stun()
+    IEnumerator stun(float time)
     {
         dragFactor = 0.99f;
         walk = false;
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(time);
         walk = true;
         dragFactor = 0.75f;
         yield break;
